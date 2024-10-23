710bba90207680009c6c1c11a744ea05d831ac8a
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-10-23 12:15:46 +0200

    added some checkout logic

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,32 @@
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index.js'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBox, faCalendar, faStar, faGraduationCap, faSpa, faTags, faComments, faLayerGroup, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index.js";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBox,
+  faCalendar,
+  faStar,
+  faGraduationCap,
+  faSpa,
+  faTags,
+  faComments,
+  faLayerGroup,
+  faShoppingBag,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { store } from "./store/store.js";
 
-library.add(faBox, faCalendar, faStar, faGraduationCap, faSpa, faTags, faComments, faLayerGroup, faShoppingBag);
-
+library.add(
+  faBox,
+  faCalendar,
+  faStar,
+  faGraduationCap,
+  faSpa,
+  faTags,
+  faComments,
+  faLayerGroup,
+  faShoppingBag
+);
 
 createApp(App)
   .component("font-awesome-icon", FontAwesomeIcon)
