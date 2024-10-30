a4fcd3f0676495b61fd01deaa94bca52d8f4c06e
lucasackerberg <lucas@ackerberg.se>
2024-10-30 10:18:50 +0100

    Update createdb.js
    asdasdasd

--- backend/config/createdb.js
+++ backend/config/createdb.js
@@ -181,6 +181,12 @@ const insertDataQueries = [
   ('Nail Services'),
   ('Hair Services');`,
 
+  `INSERT INTO productSizes (product_id, size, price, stock_quantity) VALUES
+    (1, '50 ml', 29.99, 150),
+    (1, '100 ml', 34.99, 100),
+    (2, '250 ml', 15.99, 200),
+    (3, '5 ml', 19.99, 100);`
+
   // Insert into Products
   `INSERT INTO Products (product_name, description, stock_quantity, category_id)
   VALUES
